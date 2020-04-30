44c10a11434d9d83ae9063c49cc308f9891bc236
Addy <aclaure@gmail.com>
2020-04-30 06:54:25 -0400

    rearranging

---
+++ src/Console/ColorScope.cpp
@@ -0,0 +1,72 @@
+#include "ColorScope.h"
+
+#include <curses.h>
+
+namespace owl
+{
+
+ColorScope::ColorScope(WINDOW* window, int colornum, bool bold)
+    : _window{ window }, _colornum{ colornum }, _bold{ bold }
+{
+    turnOnAttributes();
+}
+
+ColorScope::~ColorScope()
+{
+    turnOffAttributes();
+}
+
+void ColorScope::reset(int colornum, bool bold)
+{
+    turnOffAttributes();
+
+    _colornum = colornum;
+    _bold = bold;
+
+    turnOnAttributes();
+    _reset = false;
+}
+
+void ColorScope::reset()
+{
+    turnOffAttributes();
+}
+
+void ColorScope::print(const std::string_view& text)
+{
+    waddstr(_window, text.data());
+}
+
+void ColorScope::printXY(int x, int y, std::string_view text)
+{
+    int origx = 0;
+    int origy = 0;
+    getyx(_window, origy, origx);
+
+    wmove(_window, y, x);
+    waddstr(_window, text.data());
+    wmove(_window, origy, origx);
+}
+
+void ColorScope::drawHorizontalLine(int x, int y, int length)
+{
+    wmove(_window, y, x);
+    whline(_window, ACS_HLINE, length);
+}
+
+void ColorScope::turnOnAttributes()
+{
+    wattron(_window, COLOR_PAIR(_colornum));
+    if (_bold) wattron(_window, A_BOLD);
+    _reset = false;
+}
+
+void ColorScope::turnOffAttributes()
+{
+    if (_reset) return;
+    wattroff(_window, COLOR_PAIR(_colornum));
+    if (_bold) wattroff(_window, A_BOLD);
+    _reset = true;
+}
+
+} // namespade
---
+++ src/Console/ColorScope.h
@@ -0,0 +1,125 @@
+#pragma once
+
+#include <map>
+#include <string_view>
+#include <string>
+
+#include <boost/range/adaptor/indexed.hpp>
+
+#include <curses.h>
+
+namespace owl
+{
+
+constexpr int O_COLOR_BLACK = 0;
+constexpr int O_COLOR_BLUE = 1;
+constexpr int O_COLOR_GREEN = 2;
+constexpr int O_COLOR_CYAN = 3;
+constexpr int O_COLOR_RED = 4;
+constexpr int O_COLOR_MAGENTA = 5;
+constexpr int O_COLOR_YELLOW = 6;
+constexpr int O_COLOR_WHITE = 7;
+constexpr int O_COLOR_GREY = 8;
+
+using ColorMap = std::map<int, std::tuple<int, int, int>>;
+const ColorMap COLOR_CODES =
+{
+    { O_COLOR_BLACK,    { 0, 0, 0 } },
+    { O_COLOR_BLUE,     { 0, 0, 1000 } },
+    { O_COLOR_GREEN,    { 0, 1000, 0 } },
+    { O_COLOR_CYAN,     { 0, 1000, 1000 } },
+    { O_COLOR_RED,      { 1000, 0, 0 } },
+    { O_COLOR_MAGENTA,  { 1000, 0, 1000 } },
+    { O_COLOR_YELLOW,   { 1000, 1000, 0 } },
+    { O_COLOR_WHITE,    { 0, 0, 0 } },
+    { O_COLOR_GREY,     { 500, 500, 500 } },
+};
+
+struct ColorPairInfo
+{
+    std::string name;
+    int         fg;
+    int         bg;
+};
+
+const ColorPairInfo DEFAULT_THEME[]
+{
+    { "", 0, 0 }, // ncurses default (skipped)
+    { "MenuBar", O_COLOR_BLACK, O_COLOR_CYAN },
+    { "WelcomeText", O_COLOR_MAGENTA, O_COLOR_BLACK },
+    { "BoxOutline", O_COLOR_YELLOW, O_COLOR_BLACK },
+    { "DebugInfo", O_COLOR_BLACK, O_COLOR_GREY },
+    { "Warning", O_COLOR_BLACK, O_COLOR_YELLOW },
+    { "Error", O_COLOR_BLACK, O_COLOR_RED },
+    { "Prompt", O_COLOR_YELLOW, O_COLOR_BLUE },
+};
+
+namespace
+{
+
+// not a great design but will do for now
+int color_pair_num(std::string_view name)
+{
+    const auto it = std::find_if(
+        std::begin(DEFAULT_THEME), std::end(DEFAULT_THEME),
+        [name](const ColorPairInfo& info)
+        {
+            return name == info.name;
+        });
+
+    if (it == std::end(DEFAULT_THEME))
+    {
+        // TODO: warning?
+        return 0;
+    }
+
+    return static_cast<int>(std::distance(std::begin(DEFAULT_THEME), it));
+}
+
+} // namespace (anonymous)
+
+class ColorScope
+{
+    WINDOW* _window = nullptr;
+    int     _colornum = 0;
+    bool    _bold = false;
+    bool    _reset = false;
+
+public:
+    ColorScope(WINDOW* window, int colornum, bool bold);
+
+    ColorScope(WINDOW* window, int colornum)
+        : ColorScope(window, colornum, false)
+    {}
+
+    explicit ColorScope(int colornum)
+        : ColorScope(stdscr, colornum)
+    {}
+
+    ColorScope(WINDOW* window, std::string_view name, bool bold)
+        : ColorScope(window, color_pair_num(name), bold)
+    {}
+
+    ColorScope(WINDOW* window, std::string_view name)
+        : ColorScope(window, name, false)
+    {}
+
+    ColorScope(std::string_view name)
+        : ColorScope(stdscr, color_pair_num(name))
+    {}
+
+    ~ColorScope();
+
+    void reset(int colornum, bool bold = false);
+    void reset();
+
+    void print(const std::string_view& text);
+    void printXY(int x, int y, std::string_view text);
+    void drawHorizontalLine(int x, int y, int length);
+
+private:
+    void turnOnAttributes();
+    void turnOffAttributes();
+};
+
+} // namespace
